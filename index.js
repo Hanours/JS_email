c7fa43c855e672b831f7ac60d9eebaa9cb8b9b8a
Hana Sedlaříková <hana.sedlarikova@gmail.com>
2023-05-26 17:45:27 +0200

    Email_1

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const inboxElm = document.querySelector("#inbox");
+
+const emails = (emails) => { inboxElm.innerHTML = emails.map((email) => 
+  `
+        <div class="email">
+          <div class="email__head">
+            <div class="email__icon email__icon--closed"></div>
+            <div class="email__info">
+              <div class="email__sender">${email.sender.name}</div>
+              <div class="email__subject">${email.subject}</div>
+            </div>
+            <div class="email__time">${email.time}</div>
+          </div>
+          <div class="email__body"></div>
+        </div>
+      `
+    ).join('');
+};
+
+fetch("https://apps.kodim.cz/daweb/trening-api/apis/emails")
+  .then((response) => response.json())
+  .then((data) => emails(data.emails));
